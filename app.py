decbe308c876c7d431178281a933cb49f1e9ca2f
Bharathi Sakthivel <42297176+bharathi1294@users.noreply.github.com>
2021-04-17 15:24:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,render_template
+from flask_cors import cross_origin
+import os
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@cross_origin()
+def home():
+    return render_template("home.html")
+
+@app.route('/user_data',methods = ["GET", "POST"])
+def post():
+    if request.method == "POST":
+        f1 = float(request.form['f1'])
+        f2 = float(request.form['f2'])
+        f3 = float(request.form['f3'])
+        f4 = float(request.form['f4'])
+        f5 = float(request.form['f5'])
+        f6 = float(request.form['f6'])
+        inputs = [f1,f2,f3,f4,f5,f6]
+        
+        model = pickle.load(open('C:\\Users\\user\\Downloads\\Chrome Downloads\\model.pkl','rb'))
+
+        predict = model.predict([inputs])
+        
+    return render_template("home.html",text=predict)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
